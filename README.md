480a7c764fbede3bdb14680b576dfdaf884f39e0
YusukeKato <yusuke.kato@rt-net.jp>
2024-07-04 14:59:12 +0900

    create package

---
+++ README.md
@@ -0,0 +1,62 @@
+# urdf_seminar
+
+[URDF入門セミナー](https://rt-net.jp/service/ros2seminar2024/)用のROS 2パッケージです。
+
+## 動作環境
+
+- Linux OS
+  - Ubuntu 22.04
+- ROS
+  - [Humble Hawksbill](https://docs.ros.org/en/humble/Installation.html)
+
+## インストール方法
+
+下記のコマンドを順に実行してください。
+
+```sh
+# Setup ROS 2 environment
+source /opt/ros/humble/setup.bash
+
+# Download urdf_seminar repositories
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
+git clone https://github.com/rt-education/urdf_seminar.git
+
+# Install dependencies
+rosdep install -r -y -i --from-paths .
+
+# Build & Install
+cd ~/ros2_ws
+colcon build --symlink-install
+source ~/ros2_ws/install/setup.bash
+```
+
+## 動作方法
+
+### RVizの起動
+
+```sh
+source ~/ros2_ws/install/setup.bash
+ros2 launch urdf_seminar crane_plus_rviz.launch.py
+# Press [Ctrl-c] to terminate.
+```
+
+### Gazeboの起動
+
+```sh
+# 1つ目のターミナル
+source ~/ros2_ws/install/setup.bash
+ros2 launch urdf_seminar crane_plus_gazebo.launch.py
+# Press [Ctrl-c] to terminate.
+
+# 2つ目のターミナル
+ros2 topic pub -1 /crane_plus_arm_controller/commands std_msgs/msg/Float64MultiArray "{data: [0.5,0.5,0.5,0.5]}"
+```
+
+## ライセンス
+
+(C) 2024 RT Corporation \<support@rt-net.jp\>
+
+各ファイルはライセンスがファイル中に明記されている場合、そのライセンスに従います。
+特に明記されていない場合は、Apache License, Version 2.0に基づき公開されています。  
+ライセンスの全文は[LICENSE](./LICENSE)または[https://www.apache.org/licenses/LICENSE-2.0](https://www.apache.org/licenses/LICENSE-2.0)から確認できます。
---
+++ launch/crane_plus_gazebo.launch.py
@@ -0,0 +1,98 @@
+# Copyright 2024 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
+
+
+def generate_launch_description():
+    xacro_path = os.path.join(
+        get_package_share_directory('urdf_seminar'), 'urdf', 'crane_plus.urdf.xacro')
+    xacro_data = xacro.process_file(xacro_path)
+    urdf_data = xacro_data.toprettyxml(indent='  ')
+
+    rsp = Node(package='robot_state_publisher',
+               executable='robot_state_publisher',
+               output='both',
+               parameters=[{'robot_description': urdf_data}])
+    
+    env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': os.environ['LD_LIBRARY_PATH'],
+           'IGN_GAZEBO_RESOURCE_PATH': os.path.dirname(
+               get_package_share_directory('urdf_seminar'))}
+
+    world_file = os.path.join(
+        get_package_share_directory('urdf_seminar'), 'worlds', 'table.sdf')
+    
+    gui_config = os.path.join(
+        get_package_share_directory('urdf_seminar'), 'config', 'gui.config')
+
+    ign_gazebo = ExecuteProcess(
+            cmd=['ign gazebo -r', world_file, '--gui-config', gui_config],
+            output='screen',
+            additional_env=env,
+            shell=True
+        )
+
+    gazebo_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-topic', '/robot_description',
+                   '-name', 'crane_plus',
+                   '-z', '1.015',
+                   '-allow_renaming', 'true'],
+    )
+
+    spawn_joint_state_controller = ExecuteProcess(
+                cmd=['ros2 run controller_manager spawner joint_state_broadcaster'],
+                shell=True,
+                output='screen',
+            )
+
+    spawn_arm_controller = ExecuteProcess(
+                cmd=['ros2 run controller_manager spawner crane_plus_arm_controller'],
+                shell=True,
+                output='screen',
+            )
+
+    spawn_gripper_controller = ExecuteProcess(
+                cmd=['ros2 run controller_manager spawner crane_plus_gripper_controller'],
+                shell=True,
+                output='screen',
+            )
+
+    bridge = Node(
+                package='ros_gz_bridge',
+                executable='parameter_bridge',
+                arguments=['/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock'],
+                output='screen'
+            )
+
+    return LaunchDescription([
+        SetParameter(name='use_sim_time', value=True),
+        rsp,
+        ign_gazebo,
+        gazebo_spawn_entity,
+        spawn_joint_state_controller,
+        spawn_arm_controller,
+        spawn_gripper_controller,
+        bridge
+    ])
