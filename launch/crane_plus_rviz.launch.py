4a900b08b84b1f3d32b2601b011ecde57ef80fdc
YusukeKato <YusukeKato@users.noreply.github.com>
2024-07-11 13:14:28 +0900

    URDF入門セミナー用のROS 2パッケージを作成 (#1)
    * create package
    
    * 正常にビルドできる状態に修正
    
    * RVizとGazeboの動作方法の説明を修正
    
    * crane_plus.urdf.xacro内でuse_gazeboフラグを使ってGazebo関連の処理を条件分岐させる+体裁を整える
    
    * コーディングスタイルを修正
    
    * コマンドのブロックにshを指定
    
    Co-authored-by: Kuwamai <mitsumex333@gmail.com>
    
    * 変数名を修正
    
    ---------
    
    Co-authored-by: Kuwamai <mitsumex333@gmail.com>

---
+++ launch/crane_plus_rviz.launch.py
@@ -0,0 +1,52 @@
+# Copyright 2024 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    xacro_path = os.path.join(
+        get_package_share_directory('urdf_seminar'), 'urdf', 'crane_plus.urdf.xacro')
+    doc = xacro.process_file(xacro_path)
+    urdf_data = doc.toprettyxml(indent='  ')
+
+    rsp = Node(package='robot_state_publisher',
+               executable='robot_state_publisher',
+               output='both',
+               parameters=[{'robot_description': urdf_data}])
+
+    jsp = Node(package='joint_state_publisher_gui',
+               executable='joint_state_publisher_gui',
+               output='screen')
+
+    rviz_config = get_package_share_directory(
+        'urdf_seminar') + '/config/crane_plus.rviz'
+
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     name='rviz2',
+                     output='log',
+                     arguments=['-d', rviz_config])
+
+    return LaunchDescription([
+        rsp,
+        jsp,
+        rviz_node,
+    ])
